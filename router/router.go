3618e427c13aa793552b75681735176ac6436197
Marie Lohbeck <marie.lohbeck@uni-ulm.de>
2021-08-11 14:01:42 +0200

    removed ip translation

--- router/router.go
+++ router/router.go
@@ -1,86 +1,87 @@
 package router
 
 import (
-    "crypto/tls"
-    "crypto/x509"
-    "fmt"
-    "net/http"
-    "io/ioutil"
-    metadata "local.com/leobrada/ztsfc_http_sfpLogic/metadata"
-    sfpl "local.com/leobrada/ztsfc_http_sfpLogic/sfp_logic"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	metadata "local.com/leobrada/ztsfc_http_sfpLogic/metadata"
+	sfpl "local.com/leobrada/ztsfc_http_sfpLogic/sfp_logic"
 )
 
 type Router struct {
-    frontend_tls_config *tls.Config
-    frontend_server *http.Server
+	frontend_tls_config *tls.Config
+	frontend_server     *http.Server
 
-    router_cert_shown_to_clients tls.Certificate
-    certs_that_router_accepts *x509.CertPool
+	router_cert_shown_to_clients tls.Certificate
+	certs_that_router_accepts    *x509.CertPool
 
-//    md         *metadata.Cp_metadata
+	//    md         *metadata.Cp_metadata
 }
 
-func NewRouter() (*Router) {
+func NewRouter() *Router {
 
-    // Create new Router
-    router := new(Router)
+	// Create new Router
+	router := new(Router)
 
-    // Initialize Certificates
-    router.router_cert_shown_to_clients, _ = tls.LoadX509KeyPair("./certs/srv2_basicAuth.crt", "./certs/srv2_basicAuth_priv.key")
-    router.certs_that_router_accepts = x509.NewCertPool()
-    ca_cert, err := ioutil.ReadFile("./certs/ca_pool.pem")
-    if err != nil {
-        fmt.Printf("[Router.makeCAPool]: ReadFile: ", err)
-        return nil
-    }
-    ok := router.certs_that_router_accepts.AppendCertsFromPEM(ca_cert)
-    if !ok {
-        fmt.Printf("[Router.makeCAPool]: AppendCertsFromPEM: ", err)
-        return nil
-    }
+	// Initialize Certificates
+	router.router_cert_shown_to_clients, _ = tls.LoadX509KeyPair("./certs/srv2_basicAuth.crt", "./certs/srv2_basicAuth_priv.key")
+	router.certs_that_router_accepts = x509.NewCertPool()
+	ca_cert, err := ioutil.ReadFile("./certs/ca_pool.pem")
+	if err != nil {
+		fmt.Print("[Router.makeCAPool]: ReadFile: ", err)
+		return nil
+	}
+	ok := router.certs_that_router_accepts.AppendCertsFromPEM(ca_cert)
+	if !ok {
+		fmt.Print("[Router.makeCAPool]: AppendCertsFromPEM: ", err)
+		return nil
+	}
 
-    // Create TLS config for frontend server
-    router.frontend_tls_config = &tls.Config{
-        Rand:   nil,
-        Time:   nil,
-        MinVersion: tls.VersionTLS13,
-        MaxVersion: tls.VersionTLS13,
-        SessionTicketsDisabled: true,
-        Certificates:   []tls.Certificate{router.router_cert_shown_to_clients},
-        ClientAuth: tls.RequireAndVerifyClientCert,
-        ClientCAs:  router.certs_that_router_accepts,
-    }
+	// Create TLS config for frontend server
+	router.frontend_tls_config = &tls.Config{
+		Rand:                   nil,
+		Time:                   nil,
+		MinVersion:             tls.VersionTLS13,
+		MaxVersion:             tls.VersionTLS13,
+		SessionTicketsDisabled: true,
+		Certificates:           []tls.Certificate{router.router_cert_shown_to_clients},
+		ClientAuth:             tls.RequireAndVerifyClientCert,
+		ClientCAs:              router.certs_that_router_accepts,
+	}
 
-    // Create MUX server
-    mux := http.NewServeMux()
-    mux.Handle("/", router)
+	// Create MUX server
+	mux := http.NewServeMux()
+	mux.Handle("/", router)
 
-    // Create HTTP frontend server
-    router.frontend_server = &http.Server {
-        Addr: "10.4.0.52:8889",
-        TLSConfig: router.frontend_tls_config,
-        Handler: mux,
-    }
+	// Create HTTP frontend server
+	router.frontend_server = &http.Server{
+		Addr:      "10.4.0.52:8889",
+		TLSConfig: router.frontend_tls_config,
+		Handler:   mux,
+	}
 
-   // router.md = new(metadata.Cp_metadata)
+	// router.md = new(metadata.Cp_metadata)
 
-    return router
+	return router
 }
 
 func (router *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-    //fmt.Printf("%+v\n", req.Header)
+	//fmt.Printf("%+v\n", req.Header)
 
-    md := new(metadata.Cp_metadata)
+	md := new(metadata.Cp_metadata)
 
-    md.ExtractMetadata(req)
+	md.ExtractMetadata(req)
 
-    sfpl.TransformSFCintoSFP(md)
+	sfpl.TransformSFCintoSFP(md)
 
-//    fmt.Printf("%s\n", md.SFP)
-    w.Header().Set("sfp", md.SFP)
-    fmt.Fprintf(w, "")
+	//    fmt.Printf("%s\n", md.SFP)
+	w.Header().Set("sfp", md.SFP)
+	fmt.Fprintf(w, "")
 }
 
 func (router *Router) ListenAndServeTLS() error {
-    return router.frontend_server.ListenAndServeTLS("", "")
+	return router.frontend_server.ListenAndServeTLS("", "")
 }
--- sfp_logic/sfp_logic.go
+++ sfp_logic/sfp_logic.go
@@ -1,39 +1,43 @@
 package sfp_logic
 
 import (
-    md "local.com/leobrada/ztsfc_http_sfpLogic/metadata"
-    "strings"
-    "fmt"
+	"fmt"
+
+	md "local.com/leobrada/ztsfc_http_sfpLogic/metadata"
 )
 
 //func TransformSFCintoSFP(cpm *md.Cp_metadata) {
 //    cpm.SFP = ""
 //}
 
-
 func TransformSFCintoSFP(cpm *md.Cp_metadata) {
-    if cpm == nil {
-        fmt.Printf("cpm is nil")
-        return
-    }
+	if cpm == nil {
+		fmt.Printf("cpm is nil")
+		return
+	}
 
-    if len(cpm.SFC) == 0 {
-        cpm.SFP = ""
-        return
-    }
+	if len(cpm.SFC) == 0 {
+		cpm.SFP = ""
+		return
+	}
 
-    sfc := strings.Split(cpm.SFC, ",")
-    sfp := ""
-    for _, sf := range sfc {
-        switch sf {
-        case "dpi":
-            sfp += ",https://10.5.0.54:8888"
-        case "logger":
-            sfp += ",https://10.5.0.50:8889"
-        default:
-            sfp += ""
-        }
-    }
-    sfp = strings.TrimLeft(sfp, ",")
-    cpm.SFP = sfp
+	// @author marie:
+	// removed translation into ip addresses, because this is currently not useful from the PEP's perspective.
+	// Thus, function simply returns SFC as SFP.
+	cpm.SFP = cpm.SFC
+
+	// sfc := strings.Split(cpm.SFC, ",")
+	// sfp := ""
+	// for _, sf := range sfc {
+	//     switch sf {
+	//     case "dpi":
+	//         sfp += ",https://10.5.0.54:8888"
+	//     case "logger":
+	//         sfp += ",https://10.5.0.50:8889"
+	//     default:
+	//         sfp += ""
+	//     }
+	// }
+	// sfp = strings.TrimLeft(sfp, ",")
+	// cpm.SFP = sfp
 }
